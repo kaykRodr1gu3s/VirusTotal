4debc25aec66e0aaf3c383b00204e8f68011342e
kaykRodr1gu3s <110197812+kaykRodr1gu3s@users.noreply.github.com>
2023-10-09 16:54:20 -0300

    changing somethings

---
+++ upload_file/upload_file.py
@@ -0,0 +1,37 @@
+import requests
+import os
+
+api_key = 'api_key'
+path = os.getcwd()+'\\python-3.11.5-amd64.exe' 
+
+def getting_link(endpoint, path):
+    global api_key
+
+
+    files = { "file": ("the executable name(for example : python-3.11.5-amd64.exe) ", open(path, "rb"), "application/x-msdownload") }
+    headers = {
+        "accept": "application/json",
+        "x-apikey": api_key
+    }
+
+
+
+    # doing the request
+
+    response = requests.post(endpoint, files=files, headers=headers)
+
+
+    datas = response.json()
+
+
+    # getting the id
+    
+    id = datas['data']['link']
+
+    id = id['self']
+
+    return id
+    
+path = ''   # the path where is your file
+endpoint = 'https://www.virustotal.com/api/v3/files'
+file = getting_link(endpoint, path)
